c8bc0d86665c371fc0192a43af6853b6c5a94c67
Michael Krukov <krukov.michael@ya.ru>
2019-05-16 15:00:52 +0300

    Removed unused import

--- app/src/main/java/com/kryukov/daysuntil/Utils.java
+++ app/src/main/java/com/kryukov/daysuntil/Utils.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.util.Log;
 
 import java.util.Calendar;
 
